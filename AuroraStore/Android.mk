c1dada480178854afb5aa44af064863965ec1db0
Sébastien NOBILI <code@pipoprods.org>
2022-09-06 09:02:09 +0200

    LOCAL_USES_LIBRARIES => LOCAL_OPTIONAL_USES_LIBRARIES

--- AuroraStore/Android.mk
+++ AuroraStore/Android.mk
@@ -6,5 +6,5 @@ LOCAL_SRC_FILES := AuroraStore_4.1.1.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
--- NeoBackup/Android.mk
+++ NeoBackup/Android.mk
@@ -6,5 +6,5 @@ LOCAL_SRC_FILES := com.machiav3lli.backup_804.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
+LOCAL_OPTIONAL_USES_LIBRARIES := androidx.window.extensions androidx.window.sidecar
 include $(BUILD_PREBUILT)
