a357492ca2559779fbb790b18e689e2ed183c16f
Sébastien NOBILI <code@pipoprods.org>
2022-12-11 16:53:17 +0100

    Neo Backup 8.2.5

--- NeoBackup/Android.mk
+++ NeoBackup/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := NeoBackup
-LOCAL_SRC_FILES := com.machiav3lli.backup_804.apk
+LOCAL_SRC_FILES := com.machiav3lli.backup_8208.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
 LOCAL_CERTIFICATE := PRESIGNED
